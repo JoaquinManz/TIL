82b19a048564c6bdc943a27e7426ff4baf0a3aba
Joaquin Manzanares <105377864+JoaquinManz@users.noreply.github.com>
2025-11-05 11:34:17 -0300

    Create hoops exercise

---
+++ codewars/hoops.js
@@ -0,0 +1,6 @@
+// Could have used an if statement, but I wanted to show how to use ternary operators
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+function hoopCount (n) {
+   //your code goes here    
+  return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+}

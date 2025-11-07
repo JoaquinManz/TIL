e011c4dc6278763aebba46270a4c7ab160413685
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-11-06 22:43:44 -0300

    How Good Are You Really? Exercise

---
+++ codewars/howGoodAreYouReally.js
@@ -0,0 +1,21 @@
+// There was a test in your class and you passed it. Congratulations!
+
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return true if you're better, else false!
+
+// Note:
+// Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+//https://www.codewars.com/kata/5601409514fc93442500010b
+
+
+function betterThanAverage(classPoints, yourPoints) {
+    //use reduce to sum the classPoints array
+  const total = classPoints.reduce((sum, score) => sum + score, yourPoints);
+//   used the function total to calculate the average.
+  const average = total / (classPoints.length + 1);
+  // if my points are greater that average, return true
+  return yourPoints > average;
+}

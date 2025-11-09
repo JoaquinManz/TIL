aa5aa8fe1d05828fd76fa1fef5d4ece99aef756d
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-11-09 19:36:38 -0300

    returning strings task

---
+++ codewars/returningStrings.js
@@ -0,0 +1,8 @@
+// Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+// https://www.codewars.com/kata/55a70521798b14d4750000a4
+
+function greet(name){
+  //your code here
+  return `Hello, ${name} how are you doing today?`
+}

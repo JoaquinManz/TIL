5518707392a90572971fbb0c0fa79f4d7908bd69
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-11-09 22:50:07 -0300

    keep hydrated task

---
+++ codewars/keepHydrated.js
@@ -0,0 +1,25 @@
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded down.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+// https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+
+function litres(time) {
+  // .5 litres per time
+  let litresDrank = 0.5 * time;
+  
+  return Math.floor(litresDrank)
+}
+
+// I did not remember about Math.floor, had to look it up.

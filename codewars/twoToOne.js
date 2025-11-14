b538736a5f4c39f2d71af45685de741787749e54
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-11-13 22:41:08 -0300

    two to one task

---
+++ codewars/twoToOne.js
@@ -0,0 +1,18 @@
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+// https://www.codewars.com/kata/5656b6906de340bd1b0000ac
+
+function longest(s1, s2) {
+  // your code
+  return [...new Set(s1 + s2)].sort().join('');
+}
+
+// used spread method, Set(), which creates a new array with all unique values of s1 + s2, then sorted it and joined it to a string

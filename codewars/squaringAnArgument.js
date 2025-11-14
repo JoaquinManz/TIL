270ab151850d9a38807741a06eb38bb820f39917
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-11-14 12:33:15 -0300

    squaring an argument task

---
+++ codewars/squaringAnArgument.js
@@ -0,0 +1,11 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// https://www.codewars.com/kata/523b623152af8a30c6000027
+
+// Write the "square"-function here
+function square(a) {
+  return a ** 2
+}
+
+// The ** operator is the power operator. It takes two arguments, the first is the base and the second is the exponent.
+// also Math.pow could be used

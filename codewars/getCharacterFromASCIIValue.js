3092f440bd89965ed0d59b25abb48ae8225d7068
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-11-17 20:40:00 -0300

    get character from ascii value task

---
+++ codewars/getCharacterFromASCIIValue.js
@@ -0,0 +1,16 @@
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0
+
+// https://www.codewars.com/kata/55ad04714f0b468e8200001c
+
+function getChar(c){
+  // ...
+  return String.fromCharCode(c)
+}
+
+// this one is an interesting one, i did not know about fromCharCode method

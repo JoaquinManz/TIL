15670c3db7947c246d8ce2bfa5b0fe4327ad2e1d
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-11-24 23:52:46 -0300

    volume of a cuboid task

---
+++ codewars/volumeOfACuboid.js
@@ -0,0 +1,12 @@
+// Bob needs a fast way to calculate the volume of a rectangular cuboid with three values: the length, width and height of the cuboid.
+
+// Write a function to help Bob with this calculation.
+
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+ class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    // your code here
+    return length * width * height
+  }
+}

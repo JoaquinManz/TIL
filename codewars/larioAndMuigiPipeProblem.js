e7c167693f22a6eddb91eba19f3fdcb9dd914126
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-12-04 00:37:40 -0300

    pipe problem task

---
+++ codewars/larioAndMuigiPipeProblem.js
@@ -0,0 +1,30 @@
+// Issue
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+// The pipes are correct when each pipe after the first is 1 more than the previous one.
+
+// Task
+// Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// Example
+// Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+// https://www.codewars.com/kata/56b29582461215098d00000f
+
+function pipeFix(numbers){
+    // create an empty array to hold the fixed pipes
+  let fixedPipes = []
+
+  // get the minimum and maximum values from the input array
+  let min = numbers[0];
+  let max = numbers[numbers.length - 1];
+  
+    // loop from min to max and push each value into the fixedPipes array
+  for(let i = min; i <= max; i++) {
+      fixedPipes.push(i)
+  }
+  
+  return fixedPipes;
+}

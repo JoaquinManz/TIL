2e74dd78e3f08675dd8cb8eb508e017bdcee2f78
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-12-15 13:02:01 -0300

    testing 123 task

---
+++ codewars/testing123.js
@@ -0,0 +1,25 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+// https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9
+
+var number=function(array){
+  //your awesome code here
+  const result = []
+  
+  for (let i = 0; i < array.length; i++) {
+    result.push(`${i + 1}: ${array[i]}`)
+  }
+  
+  return result
+}
+
+// we loop through the given array and push the index into the result array adding the colon and space

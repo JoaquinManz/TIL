ab60b980f7dfad93a651ad703e0b0dd5bdeb07c6
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-12-17 13:14:56 -0300

    name shuffler task

---
+++ codewars/nameShuffler.js
@@ -0,0 +1,12 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+// https://www.codewars.com/kata/559ac78160f0be07c200005a
+
+function nameShuffler(str){
+  //Shuffle It
+  return str.split(" ").reverse().join(" ")
+}
